3b9b66ce18ea33606cfd35fc57d62bd6d3ce2275
Meridiano <megametallist@gmail.com>
2024-07-10 09:37:58 +0300

    SlowTimeSFSE 1.6.0

---
+++ SlowTimeSFSE/src/pch.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "SFSE/SFSE.h"
+#include "RE/Starfield.h"
+
+namespace logs = SFSE::log;
+using namespace std::literals;
+
+// required
+#include <windows.h>
+
+// ini reader
+#include "mini/ini.h"
+
+// sound lib
+#pragma comment(lib, "winmm.lib")
---
+++ SlowTimeSFSE/xmake.lua
@@ -0,0 +1,47 @@
+-- set minimum xmake version
+set_xmakever("2.8.2")
+
+-- add custom package repository
+add_repositories("re https://github.com/Starfield-Reverse-Engineering/commonlibsf-xrepo")
+
+-- set project
+set_project("SlowTimeSFSE")
+set_version("1.6.0")
+set_license("MIT")
+
+-- set defaults
+set_languages("c++23")
+set_optimize("faster")
+-- disable warnings
+-- set_warnings("allextra", "error")
+set_defaultmode("releasedbg")
+
+-- add rules
+add_rules("mode.releasedbg", "mode.debug")
+add_rules("plugin.vsxmake.autoupdate")
+
+-- require package dependencies
+add_requires("commonlibsf")
+
+-- setup targets
+target("SlowTimeSFSE")
+    -- bind package dependencies
+    add_packages("commonlibsf")
+
+    -- add commonlibsf plugin
+    add_rules("@commonlibsf/plugin", {
+        name = "SlowTimeSFSE",
+        author = "Meridiano",
+        description = "Slow Time SFSE DLL Plugin",
+        email = "megametallist@gmail.com",
+        options = {
+            address_library = true,
+            no_struct_use = true
+        }
+    })
+
+    -- add source files
+    add_files("src/*.cpp")
+    add_headerfiles("src/*.h")
+    add_includedirs("src")
+    set_pcxxheader("src/pch.h")
