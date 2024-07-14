c871d341ab5b49431f826d0029f71ad5e28d62b2
Meridiano <megametallist@gmail.com>
2024-07-14 19:20:00 +0300

    StartupBlockInput 2.0.0 (A)
    C++ code update

--- StartupBlockInput/src/main.cpp
+++ StartupBlockInput/src/main.cpp
@@ -1,34 +1,30 @@
 namespace SBIUtility {
 
-	STARTUPINFO si = { sizeof(STARTUPINFO) };
-	PROCESS_INFORMATION pi;
-	LPCTSTR path = "Data\\SFSE\\Plugins\\StartupBlockInput.exe";
-	BOOL success = false;
+	bool locked = false;
 
-	void LOCK() {
-		success = CreateProcessA(
-			path,              // The path
-			NULL,              // Command line
-			NULL,              // Process handle not inheritable
-			NULL,              // Thread handle not inheritable
-			FALSE,             // Set handle inheritance to FALSE
-			NULL,              // No creation flags
-			NULL,              // Use parent's environment block
-			NULL,              // Use parent's starting directory
-			std::addressof(si),// Pointer to STARTUPINFO structure
-			std::addressof(pi) // Pointer to PROCESS_INFORMATION structure
-		);
-		logs::info("SBIUtility.LOCK = {}", success);
+	bool ShowHideMenu(std::string menuName, bool newState) {
+		if (auto sfui = RE::UI::GetSingleton(); sfui) {
+			if (sfui->IsMenuOpen(menuName) == newState) return true;
+			if (auto msgQueue = RE::UIMessageQueue::GetSingleton(); msgQueue) {
+				RE::UIMessage newMsg = newState ? RE::UIMessage::kShow : RE::UIMessage::kHide;
+				auto msgHandler = msgQueue->AddMessage(menuName, newMsg);
+				return (msgHandler != 0);
+			}
+		}
+		return false;
 	}
 
-	void UNLOCK() {
-		if (success) {
-			BOOL a = TerminateProcess(pi.hProcess, 0);
-			BOOL b = TerminateProcess(pi.hThread, 0);
-			BOOL c = CloseHandle(pi.hProcess);
-			BOOL d = CloseHandle(pi.hThread);
-			logs::info("SBIUtility.UNLOCK = {}|{}|{}|{}", a, b, c, d);
-		} else logs::info("SBIUtility.UNLOCK = Disabled");
+	void Lock(std::string source) {
+		locked = ShowHideMenu("LoadingMenu", true);
+		logs::info("SBIUtility.Lock({}) = {:X}", source, locked);
+	}
+
+	void Unlock(std::string source) {
+		if (locked) {
+			bool unlocked = ShowHideMenu("LoadingMenu", false);
+			logs::info("SBIUtility.Unlock({}) = {:X}", source, unlocked);
+			locked = !unlocked;
+		} else logs::info("SBIUtility.Unlock({}) = Disabled", source);
 	}
 
 	std::uint32_t UInt32MinMax(std::uint32_t val, std::uint32_t min, std::uint32_t max) {
@@ -39,35 +35,69 @@ namespace SBIUtility {
 
 }
 
+namespace SBIConfig {
+
+	std::uint32_t iDelayMS;
+	bool bTimeOut;
+	std::uint32_t iTimeOutMS;
+
+	void ReadConfig() {
+		// set default
+		iDelayMS = 1000;
+		bTimeOut = false;
+		iTimeOutMS = 120000;
+		// override
+		mINI::INIFile file("Data\\SFSE\\Plugins\\StartupBlockInput.ini");
+		mINI::INIStructure ini;
+		if (file.read(ini)) {
+			std::string raw;
+			try {
+				// iDelayMS
+				raw = ini.get("General").get("iDelayMS");
+				iDelayMS = std::stol(raw, nullptr, 0);
+				iDelayMS = SBIUtility::UInt32MinMax(iDelayMS, 1, 65535);
+				// bTimeout
+				raw = ini.get("General").get("bTimeOut");
+				bTimeOut = (std::stol(raw, nullptr, 0) != 0);
+				// iTimeOutMS
+				raw = ini.get("General").get("iTimeOutMS");
+				iTimeOutMS = std::stol(raw, nullptr, 0);
+				iTimeOutMS = SBIUtility::UInt32MinMax(iTimeOutMS, 5000, 600000);
+			} catch (...) {
+				logs::info("ReadConfig > Conversion issue");
+			}
+		} else {
+			logs::info("ReadConfig > File path issue");
+		}
+		logs::info("SBIConfig.iDelayMS = {}", iDelayMS);
+		logs::info("SBIConfig.bTimeOut = {:X}", bTimeOut);
+		logs::info("SBIConfig.iTimeOutMS = {}", iTimeOutMS);
+	}
+
+}
+
 namespace SBIProcess {
 
 	void DelayThread(std::uint32_t sleepTime) {
 		Sleep(sleepTime);
-		SBIUtility::UNLOCK();
+		SBIUtility::Unlock("DelayThread");
+	}
+
+	void TimeoutThread(std::uint32_t sleepTime) {
+		Sleep(sleepTime);
+		SBIUtility::Unlock("TimeoutThread");
 	}
 
 	void MessageCallback(SFSE::MessagingInterface::Message* a_msg) noexcept {
 		if (a_msg->type == SFSE::MessagingInterface::kPostDataLoad) {
-			// define delay
-			std::uint32_t delay = 0;
-			// read delay
-			mINI::INIFile file("Data\\SFSE\\Plugins\\StartupBlockInput.ini");
-			mINI::INIStructure ini;
-			if (file.read(ini)) {
-				try {
-					std::string raw = ini.get("General").get("iDelayMS");
-					delay = std::stol(raw, nullptr, 0);
-					delay = SBIUtility::UInt32MinMax(delay, 1, 65535);
-				} catch (...) { delay = 1000; }
-			} else { delay = 1000; }
-			logs::info("Data loaded, iDelayMS = {}", delay);
-			std::thread dt(DelayThread, delay);
+			logs::info("Data loaded, creating DelayThread");
+			std::thread dt(DelayThread, SBIConfig::iDelayMS);
 			dt.detach();
 		} else return;
 	}
 
 	class EventHandler final:
-		public RE::BSTEventSink<RE::MenuOpenCloseEvent> {
+		public RE::BSTEventSink<RE::MenuOpenCloseEvent>{
 	public:
 		static EventHandler* GetSingleton() {
 			static EventHandler self;
@@ -75,11 +105,17 @@ namespace SBIProcess {
 		}
 		RE::BSEventNotifyControl ProcessEvent(const RE::MenuOpenCloseEvent& a_event, RE::BSTEventSource<RE::MenuOpenCloseEvent>* a_source) {
 			// resolve menu
-			bool matchName = stricmp(a_event.menuName.data(), "FaderMenu") == 0;
-			bool matchOpen = a_event.opening;
+			bool match = (a_event.menuName == "MainMenu") && a_event.opening;
 			// process lock
-			if (matchName && matchOpen) {
-				SBIUtility::LOCK();
+			if (match) {
+				SBIUtility::Lock("EventHandler");
+				// setup time-out
+				if (SBIConfig::bTimeOut) {
+					logs::info("Lock called, creating TimeoutThread");
+					std::thread tt(TimeoutThread, SBIConfig::iTimeOutMS);
+					tt.detach();
+				}
+				// unregister
 				auto sfui = RE::UI::GetSingleton();
 				auto handler = EventHandler::GetSingleton();
 				if (sfui && handler) sfui->UnregisterSink(handler);
@@ -116,6 +152,7 @@ SFSEPluginLoad(const SFSE::LoadInterface* a_sfse) {
 	} else {
 		SFSE::stl::report_and_fail("UI events sink not registered");
 	}
+	SBIConfig::ReadConfig();
 	// done
 	return true;
 }
