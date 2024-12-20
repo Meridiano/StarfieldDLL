8269d27fc4dc1f003292cbcd6bc4a8b3ffafd8fa
Meridiano <megametallist@gmail.com>
2024-12-20 05:19:13 +0300

    CCR(U) 1.5.0

--- ConsoleCommandRunner/src/hpp/functions.hpp
+++ ConsoleCommandRunner/src/hpp/functions.hpp
@@ -2,9 +2,10 @@
 
 #include "hpp/utility.hpp"
 
-static std::vector<std::string>                                        dataLoadedMap;
-static std::map<int, std::vector<std::string>>                         gameLoadedMap;
-static std::map<std::pair<std::string, int>, std::vector<std::string>> menuActionMap;
+static StrVec dataLoadedMap;
+static std::map<int, StrVec> gameLoadedMap;
+static std::map<std::pair<std::string, int>, StrVec> menuActionMap;
+static std::map<std::pair<std::string, StrVec>, CCRUtility::Triplet<StrVec, StrVec, StrVec>> equipItemMap;
 
 namespace CCRFunctions {
 
@@ -58,6 +59,51 @@ namespace CCRFunctions {
                                 if (menuActionMap.contains(menuAction)) menuActionMap.at(menuAction).push_back(command);
                                 else menuActionMap.insert_or_assign(menuAction, std::vector(1, command));
                             }
+                        } else if (eventType == "OnItemEquipped") {
+                            auto isEquippingData = eventTable["abEquipping"].as_boolean();
+                            auto actorStringData = eventTable["asActorString"].as_string();
+                            auto itemStringsData = eventTable["aItemStrings"].as_array();
+                            int isEquipping = isEquippingData ? isEquippingData->get() : -1;
+                            std::string actorString = actorStringData ? actorStringData->get() : "";
+                            auto itemStrings = CCRUtility::TomlArrayToVector(itemStringsData);
+                            std::pair equipElement(actorString, itemStrings);
+                            toml::array* comArray = eventTable["Commands"].as_array();
+                            if (comArray) for (toml::node& com : *comArray) {
+                                std::string command = com.as_string()->get();
+                                logs::info("{} with options [ {} {} {} ] store >> {}", eventType, isEquipping, actorString, CCRUtility::VectorToString(itemStrings), command);
+                                if (equipItemMap.contains(equipElement)) {
+                                    auto& equip = equipItemMap.at(equipElement).pos;
+                                    auto& unequip = equipItemMap.at(equipElement).neg;
+                                    auto& undefined = equipItemMap.at(equipElement).unk;
+                                    switch (isEquipping) {
+                                        case 1:
+                                            equip.push_back(command);
+                                            break;
+                                        case 0:
+                                            unequip.push_back(command);
+                                            break;
+                                        default:
+                                            undefined.push_back(command);
+                                            break;
+                                    }
+                                } else {
+                                    StrVec equip;
+                                    StrVec unequip;
+                                    StrVec undefined;
+                                    switch (isEquipping) {
+                                        case 1:
+                                            equip.push_back(command);
+                                            break;
+                                        case 0:
+                                            unequip.push_back(command);
+                                            break;
+                                        default:
+                                            undefined.push_back(command);
+                                            break;
+                                    }
+                                    equipItemMap.insert_or_assign(equipElement, CCRUtility::Triplet(equip, unequip, undefined));
+                                }
+                            }
                         }
                     }
                 }
@@ -98,6 +144,34 @@ namespace CCRFunctions {
                     logs::info("{} with options [ {} {} ] execute >> {}", "OnMenuOpenCloseEvent", menuName, isOpening, command);
                     CCRUtility::ConsoleExecute(command);
                 }
+            }
+        }
+    }
+
+    void RunEquipCommands(StrVec vActor, StrVec vItem, bool bEquipping) {
+        for (auto mapEntry : equipItemMap) {
+            auto storedStrings = mapEntry.first;
+            auto actorString = storedStrings.first;
+            auto itemStrings = storedStrings.second;
+            bool matchActor = (CCRUtility::VectorSearch(vActor).Contains(actorString) || actorString == "");
+            bool matchItem = itemStrings.empty() || [](StrVec a1, StrVec a2) {
+                auto a2s = CCRUtility::VectorSearch(a2);
+                for (auto a1s : a1) if (!a2s.Contains(a1s)) return false;
+                return true;
+            } (itemStrings, vItem);
+            if (matchActor && matchItem) {
+                auto commandTriplet = mapEntry.second;
+                auto ExecVector = [](StrVec commandArray, StrVec info) {
+                    for (std::string command : commandArray) {
+                        if (command.empty()) continue;
+                        logs::info("{} with options [ {} {} {} ] execute >> {}", "OnItemEquipped", info[0], info[1], info[2], command);
+                        CCRUtility::ConsoleExecute(command);
+                    }
+                };
+                auto itemStringsMerged = CCRUtility::VectorToString(itemStrings);
+                ExecVector(commandTriplet.unk, { "-1", actorString, itemStringsMerged });
+                if (bEquipping) ExecVector(commandTriplet.pos, { "1", actorString, itemStringsMerged });
+                else ExecVector(commandTriplet.neg, { "0", actorString, itemStringsMerged });
             }
         }
     }
--- ConsoleCommandRunner/xmake.lua
+++ ConsoleCommandRunner/xmake.lua
@@ -6,7 +6,7 @@ includes("lib/commonlibsf")
 
 -- set project
 set_project("ConsoleCommandRunner")
-set_version("1.4.8")
+set_version("1.5.0")
 set_license("MIT")
 
 -- set defaults
