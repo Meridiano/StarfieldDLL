ff3e25141e22dd1a950fe0cec222ad8d3698c337
Meridiano <megametallist@gmail.com>
2025-05-30 18:34:07 +0300

    EasyCraft 2.2.0

--- EasyCraft/src/main.cpp
+++ EasyCraft/src/main.cpp
@@ -5,7 +5,7 @@ namespace EZCUtility {
 			auto sName = RE::BSFixedString(a_name);
 			auto iOffset = std::int32_t(a_offset & 0xFFFFFF);
 			using type = RE::TESForm*(*)(std::int64_t, std::int64_t, std::int64_t, std::int32_t, RE::BSFixedString*);
-			static REL::Relocation<type> func{ REL::ID(171055) };
+			static REL::Relocation<type> func{ REL::ID(117382) };
 			return func(NULL, NULL, NULL, iOffset, &sName);
 		}
 		return nullptr;
@@ -31,7 +31,7 @@ namespace EZCUtility {
 		T val = T{};
 		bool suc = true;
 		while (suc) try {
-	#define TRY_TYPE(TYPE, FUNC) if constexpr (std::is_same<T, TYPE>::value) { val = FUNC; break; }
+			#define TRY_TYPE(TYPE, FUNC) if constexpr (std::is_same<T, TYPE>::value) { val = FUNC; break; }
 			TRY_TYPE(bool, StringToBool(raw));
 			TRY_TYPE(std::int64_t, std::stoll(raw, nullptr, 0));
 			TRY_TYPE(std::uint64_t, std::stoull(raw, nullptr, 0));
@@ -44,12 +44,73 @@ namespace EZCUtility {
 			TRY_TYPE(float, std::stof(raw, nullptr));
 			TRY_TYPE(double, std::stod(raw, nullptr));
 			TRY_TYPE(std::string, raw);
-	#undef TRY_TYPE
+			#undef TRY_TYPE
 			throw std::exception("unknown template type");
 		} catch (...) { suc = false; }
 		return std::pair(suc, val);
 	}
 
+	std::string StringToLower(std::string str) {
+		std::string out = str;
+		std::transform(
+			str.begin(), str.end(), out.begin(),
+			[](unsigned char uch) { return std::tolower(uch); }
+		);
+		return out;
+	}
+
+	template <typename T>
+	T* GetMember(const void* base, std::ptrdiff_t offset) {
+		auto address = std::uintptr_t(base) + offset;
+		auto reloc = REL::Relocation<T*>(address);
+		return reloc.get();
+	};
+
+	RE::TESFile* LookupPlugin(std::uint8_t type, std::uint32_t id) {
+		if (static auto tesDH = RE::TESDataHandler::GetSingleton(); tesDH) {
+			using list = RE::BSTArray<RE::TESFile*>;
+			switch (type) {
+				case 0: {
+					auto full = GetMember<list>(tesDH, 0x1550);
+					if (full->size() > id) return full->operator[](id);
+				}   break;
+				case 1: {
+					auto small = GetMember<list>(tesDH, 0x1560);
+					if (small->size() > id) return small->operator[](id);
+				}   break;
+				case 2: {
+					auto medium = GetMember<list>(tesDH, 0x1570);
+					if (medium->size() > id) return medium->operator[](id);
+				}   break;
+			}
+		}
+		return nullptr;
+	}
+
+	std::string FormToString(RE::TESForm* form) {
+		if (form) {
+			RE::TESFile* plugin = nullptr;
+			auto id = form->formID;
+			switch (id >> 24) {
+				case 0xFF:
+					break;
+				case 0xFE:
+					plugin = LookupPlugin(1, (id >> 12) & 0xFFF);
+					id = id & 0xFFF;
+					break;
+				case 0xFD:
+					plugin = LookupPlugin(2, (id >> 16) & 0xFF);
+					id = id & 0xFFFF;
+					break;
+				default:
+					plugin = LookupPlugin(0, id >> 24);
+					id = id & 0xFFFFFF;
+					break;
+			}
+			if (plugin) return std::format(FormString, StringToLower(plugin->fileName), id);
+		}
+		return "";
+	}
 }
 
 namespace EZCSettings {
@@ -65,17 +126,16 @@ namespace EZCSettings {
 		if (auto map = ini.get(section); map.has(key)) {
 			std::string raw = map.get(key);
 			if (auto temp = EZCUtility::ConvertTo<T>(raw); temp.first) result = temp.second;
-			else logs::info("Failed to read [{}]{} config option", section, key);
-		} else logs::info("Config option [{}]{} not found", section, key);
-		logs::info("Config option [{}]{} = {}", section, key, result);
+			else REX::INFO("Failed to read [{}]{} config option", section, key);
+		} else REX::INFO("Config option [{}]{} not found", section, key);
+		REX::INFO("Config option [{}]{} = {}", section, key, result);
 		return result;
 	}
 
 	void LoadConfig() {
 		mINI::INIFile file("Data\\SFSE\\Plugins\\EasyCraft.ini");
-		mINI::INIStructure ini;
-		if (file.read(ini)) {
-	#define CONFIG(V, S, K) V = Config<decltype(V)>(ini, S, K, V)
+		if (mINI::INIStructure ini; file.read(ini)) {
+			#define CONFIG(V, S, K) V = Config<decltype(V)>(ini, S, K, V)
 			// craft
 			CONFIG(bCraftEnabled, "General", "bCraftEnabled");
 			// research
@@ -83,32 +143,30 @@ namespace EZCSettings {
 			// credits
 			CONFIG(sCreditsPlugin, "General", "sCreditsPlugin");
 			CONFIG(iCreditsID, "General", "iCreditsID");
-	#undef CONFIG
-		} else logs::info("Config read error, all settings disabled");
+			#undef CONFIG
+		} else REX::INFO("Config read error, all settings disabled");
 	}
 
 }
 
 namespace EZCProcess {
 
-	std::set<RE::TESForm*> exceptions;
+	std::set<std::string> exceptions;
+	std::uint32_t creditsError = 0;
 
-	void SetupExceptions(RE::TESDataHandler* tesDH) {
-		exceptions.clear();
-		if (!tesDH) return;
+	void SetupExceptions() {
 		fs::path dirPath = "Data/SFSE/Plugins";
 		if (fs::exists(dirPath)) {
 			std::string type = ".ini";
 			for (fs::directory_entry fileEntry : fs::directory_iterator(dirPath)) {
 				fs::path filePath = fileEntry.path();
-				if (fs::is_regular_file(filePath) && filePath.extension() == type) {
+				if (fileEntry.is_regular_file() && filePath.extension() == type) {
 					auto fileName = filePath.filename().string();
 					if (fileName.length() > 24 && strnicmp(fileName.data(), "EasyCraft.Exception.", 20) == 0) {
-						logs::info("Reading exception file >> {}", fileName);
+						REX::INFO("Reading exception file >> {}", fileName);
 						// read ini
-						mINI::INIFile file(filePath.string());
-						mINI::INIStructure ini;
-						if (file.read(ini)) {
+						mINI::INIFile file(filePath);
+						if (mINI::INIStructure ini; file.read(ini)) {
 							for (auto sectionIterator : ini) {
 								auto section = sectionIterator.first;
 								for (auto keyIterator : sectionIterator.second) {
@@ -117,16 +175,17 @@ namespace EZCProcess {
 									bool badValue = true;
 									if (auto temp = EZCUtility::ConvertTo<std::uint32_t>(value); temp.first) {
 										std::uint32_t valueUInt32 = temp.second;
-										if (auto form = EZCUtility::GetFormFromFile(section, valueUInt32); form) {
-											exceptions.insert(form);
-											logs::info("Exception added >> {}.{:X}", RE::FormTypeToString(form->GetFormType()), form->formID);
+										auto element = std::format(FormString, section, valueUInt32);
+										if (exceptions.count(element) == 0) {
+											exceptions.insert(element);
+											REX::INFO("Exception added >> {}", element);
 											badValue = false;
 										}
 									}
-									if (badValue) logs::info("Bad value >> {}|{}|{}", section, key, value);
+									if (badValue) REX::INFO("Bad value >> {}|{}|{}", section, key, value);
 								}
 							}
-						} else logs::info("Bad ini-file structure >> {}", fileName);
+						} else REX::INFO("Bad ini-file structure >> {}", fileName);
 					}
 				}
 			}
@@ -138,115 +197,129 @@ namespace EZCProcess {
 		if (comp && comp->size() > 0) {
 			if (auto form = EZCUtility::GetFormFromFile(EZCSettings::sCreditsPlugin, EZCSettings::iCreditsID); form) {
 				comp->clear();
-				RE::BGSTypedFormValuePair::SharedVal shared = { 1 };
-				comp_t credit = { form, nullptr, shared };
+				RE::BGSTypedFormValuePair::SharedVal shared{ 1 };
+				comp_t credit{ form, nullptr, shared };
 				comp->push_back(credit);
 				return true;
+			} else {
+				creditsError += 1;
 			}
 		}
 		return false;
 	}
 
-	std::uint32_t ProcessCOBJ(RE::TESDataHandler* tesDH) {
-		std::uint32_t result = 0;
-		if (tesDH) {
-			auto& cobjArray = tesDH->formArrays[0x97];
-			cobjArray.lock.lock_write();
-			for (auto& niPtr : cobjArray.formArray) {
-				if (auto cobj = niPtr.get()->As<RE::BGSConstructibleObject>(); cobj) {
-					if (exceptions.contains(cobj)) continue;
-					if (auto comp = cobj->components; SetOneCredit(comp)) result += 1;
-				}
-			}
-			cobjArray.lock.unlock_write();
+	std::uint32_t cobjCount = 0;
+	std::uint32_t rspjCount = 0;
+	std::uint32_t cobjCountExcept = 0;
+	std::uint32_t rspjCountExcept = 0;
+
+	bool ProcessSingleCOBJ(RE::BGSConstructibleObject* cobj) {
+		auto str = EZCUtility::FormToString(cobj);
+		if (exceptions.contains(str)) {
+			cobjCountExcept += 1;
+			return false;
 		}
-		return result;
+		return SetOneCredit(cobj->components);
 	}
 
-	std::uint32_t ProcessRSPJ(RE::TESDataHandler* tesDH) {
-		std::uint32_t result = 0;
-		if (tesDH) {
-			auto& rspjArray = tesDH->formArrays[0xC1];
-			rspjArray.lock.lock_write();
-			for (auto& niPtr : rspjArray.formArray) {
-				if (auto rspj = niPtr.get()->As<RE::BGSResearchProjectForm>(); rspj) {
-					if (exceptions.contains(rspj)) continue;
-					if (auto comp = rspj->components; SetOneCredit(comp)) result += 1;
-				}
-			}
-			rspjArray.lock.unlock_write();
+	bool ProcessSingleRSPJ(RE::BGSResearchProjectForm* rspj) {
+		auto str = EZCUtility::FormToString(rspj);
+		if (exceptions.contains(str)) {
+			rspjCountExcept += 1;
+			return false;
 		}
-		return result;
+		return SetOneCredit(rspj->components);
 	}
 
-	void ProcessForms(const char* src) {
-		logs::info("ProcessForms:{}", src);
-		auto tesDataHandler = RE::TESDataHandler::GetSingleton();
-		SetupExceptions(tesDataHandler);
-		// do cobj
-		if (EZCSettings::bCraftEnabled) {
-			auto cobjCount = ProcessCOBJ(tesDataHandler);
-			logs::info("COBJ / Total affected count = {}", cobjCount);
-		}
-		// do rspj
-		if (EZCSettings::bResearchEnabled) {
-			auto rspjCount = ProcessRSPJ(tesDataHandler);
-			logs::info("RSPJ / Total affected count = {}", rspjCount);
-		}
+	void ReportAndReset(const char* arg) {
+		REX::INFO("Report:{}", arg);
+		if (creditsError) REX::INFO("Credits form not found in {} attempt(s)", creditsError);
+		REX::INFO("COBJ / Total {} affected / With {} exception(s)", cobjCount, cobjCountExcept);
+		REX::INFO("RSPJ / Total {} affected / With {} exception(s)", rspjCount, rspjCountExcept);
+		creditsError = cobjCount = rspjCount = cobjCountExcept = rspjCountExcept = 0;
 	}
 
-	// ID 148887 + Offset 17XX = Call ID 148635
-	class ReloadHook {
+	class COBJ_Hook {
 	private:
-		struct ReloadCall {
-			static std::int64_t thunk(std::int64_t in) {
-				// original
-				auto out = func(in);
-				// custom
-				ProcessForms("DataReloaded");
-				// done
-				return out;
+		struct Call {
+			static void NEW(RE::BGSConstructibleObject* cobj) {
+				OLD(cobj);
+				cobjCount += ProcessSingleCOBJ(cobj);
 			}
-			static inline REL::Relocation<decltype(thunk)> func;
+			static inline REL::Relocation<decltype(NEW)> OLD;
 		};
 	public:
 		static void Install() {
-			const REL::Relocation target{ REL::ID(148887), 0x17A8 };
-			SFSE::stl::write_thunk_call<ReloadCall>(target.address());
-			logs::info("Reload hook installed");
+			REL::Relocation reloc { REL::ID(410228) };
+			Call::OLD = reloc.write_vfunc(0x1F, Call::NEW);
+			REX::INFO("COBJ hook installed");
+		}
+	};
+
+	class RSPJ_Hook {
+	private:
+		struct Call {
+			static void NEW(RE::BGSResearchProjectForm* rspj) {
+				OLD(rspj);
+				rspjCount += ProcessSingleRSPJ(rspj);
+			}
+			static inline REL::Relocation<decltype(NEW)> OLD;
+		};
+	public:
+		static void Install() {
+			REL::Relocation reloc{ REL::ID(405103) };
+			Call::OLD = reloc.write_vfunc(0x1F, Call::NEW);
+			REX::INFO("RSPJ hook installed");
+		}
+	};
+
+	class ReloadHook {
+	private:
+		struct Call {
+			static std::int64_t NEW() {
+				ReportAndReset("DataReloaded");
+				return OLD();
+			}
+			static inline REL::Relocation<decltype(NEW)> OLD;
+		};
+	public:
+		static void Install() {
+			REL::Relocation reloc{ REL::ID(99468), 0x1907 };
+			Call::OLD = reloc.write_call<5>(Call::NEW);
+			REX::INFO("Reload hook installed");
 		}
 	};
 
 	void MessageCallback(SFSE::MessagingInterface::Message* a_msg) noexcept {
 		if (a_msg->type == SFSE::MessagingInterface::kPostLoad) {
+			EZCProcess::SetupExceptions();
+			if (EZCSettings::bCraftEnabled) COBJ_Hook::Install();
+			if (EZCSettings::bResearchEnabled) RSPJ_Hook::Install();
 			ReloadHook::Install();
 		} else if (a_msg->type == SFSE::MessagingInterface::kPostDataLoad) {
-			ProcessForms("DataLoaded");
+			ReportAndReset("DataLoaded");
 		}
 	}
 
 }
 
 SFSEPluginLoad(const SFSE::LoadInterface* a_sfse) {
-	SFSE::Init(a_sfse, false);
+	SFSE::InitInfo info{
+		.logPattern = "%d.%m.%Y %H:%M:%S [%s:%#] %v",
+		.trampoline = true,
+		.trampolineSize = 64
+	};
+	SFSE::Init(a_sfse, info);
 	SFSE::AllocTrampoline(32);
 
-	logs::init();
-	spdlog::set_pattern("%d.%m.%Y %H:%M:%S [%s:%#] %v");
+	const auto gameInfo = a_sfse->RuntimeVersion().string(".");
+	REX::INFO("Starfield v{}", gameInfo);
 
-	const auto pluginInfo = SFSE::PluginVersionData::GetSingleton();
-	logs::info(
-		"{} version {} is loading into Starfield {}",
-		std::string(pluginInfo->pluginName),
-		REL::Version::unpack(pluginInfo->pluginVersion).string("."),
-		a_sfse->RuntimeVersion().string(".")
-	);
-
-	const auto sfseMessagingInterface = SFSE::GetMessagingInterface();
-	if (sfseMessagingInterface && sfseMessagingInterface->RegisterListener(EZCProcess::MessageCallback)) {
-		logs::info("Message listener registered");
+	const auto messagingInterface = SFSE::GetMessagingInterface();
+	if (messagingInterface && messagingInterface->RegisterListener(EZCProcess::MessageCallback)) {
+		REX::INFO("Message listener registered");
 	} else {
-		SFSE::stl::report_and_fail("Message listener not registered");
+		REX::FAIL("Message listener not registered");
 	}
 
 	EZCSettings::LoadConfig();
--- EasyCraft/src/pch.h
+++ EasyCraft/src/pch.h
@@ -3,8 +3,11 @@
 #include "SFSE/SFSE.h"
 #include "RE/Starfield.h"
 
-namespace logs = SFSE::log;
 namespace fs = std::filesystem;
 using namespace std::literals;
 
 #include "../lib/mini/ini.h"
+
+static const std::uint8_t COBJ = 0x97;
+static const std::uint8_t RSPJ = 0xC1;
+#define FormString "{}:0x{:X}"
