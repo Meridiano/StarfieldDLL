7545504e9b34c504c09c1c5c3e8969a68ce39685
Meridiano <megametallist@gmail.com>
2026-04-22 07:32:20 +0300

    EasyCraft 2.5.1:A

--- EasyCraft/src/pch.h
+++ EasyCraft/src/pch.h
@@ -15,5 +15,6 @@ static const std::uint8_t LGDI = 0xCF;
 #define ThreadID std::bit_cast<std::uint32_t>(std::this_thread::get_id())
 #define SleepFor(MS) std::this_thread::sleep_for(std::chrono::milliseconds(MS))
 
-using Component = RE::BSTTuple3<RE::TESForm*, RE::BGSCurveForm*, RE::BGSTypedFormValuePair::SharedVal>;
+using ComponentCount = RE::BGSTypedFormValuePair::SharedVal;
+using Component = RE::BSTTuple3<RE::TESForm*, RE::BGSCurveForm*, ComponentCount>;
 using ComponentList = RE::BSTArray<Component>;
