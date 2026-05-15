9b6af03ce409ff053cad793d8a5ee467b6b0b842
Meridiano <megametallist@gmail.com>
2026-05-15 22:05:37 +0300

    PlayerPayCrimeGoldTweak 1.5.0

--- PlayerPayCrimeGoldTweak/src/main.cpp
+++ PlayerPayCrimeGoldTweak/src/main.cpp
@@ -13,40 +13,92 @@ namespace PPCGUtility {
         return SetCrimeGoldNative(actor, faction, violent, value);
     }
 
+    std::vector<std::string> Split(std::string input, char delim) {
+        std::vector<std::string> result;
+        std::stringstream stream(input);
+        std::string item;
+        while (std::getline(stream, item, delim)) result.push_back(item);
+        return result;
+    }
+
+    template <typename T>
+    T* GetMember(const void* base, std::ptrdiff_t offset) {
+        auto address = std::uintptr_t(base) + offset;
+        REL::Relocation<T*> reloc{ address };
+        return reloc.get();
+    }
+
+    std::string GetPluginName(RE::TESForm* arg) {
+        auto tesDH = RE::TESDataHandler::GetSingleton();
+        if (!tesDH) REX::FAIL("TESDataHandler not found");
+        // lists
+        using FL = RE::BSTArray<RE::TESFile*>;
+        auto listFull = GetMember<FL>(tesDH, 0x1580);
+        auto listSmall = GetMember<FL>(tesDH, 0x1590);
+        auto listMedium = GetMember<FL>(tesDH, 0x15A0);
+        // lambdas
+        auto GetIndex = [](RE::TESForm* form) {
+            std::uint32_t id = form ? form->formID : 0;
+            switch (id >> 24) {
+                case 0xFE: return std::pair(1, (id >> 12) & 0xFFF);
+                case 0xFD: return std::pair(2, (id >> 16) & 0xFF);
+            }
+            return std::pair(0, id >> 24);
+        };
+        auto GetName = [](FL* list, std::uint32_t index) {
+            std::string result("");
+            if (index < list->size())
+                if (auto file = list->operator[](index); file)
+                    result = file->fileName;
+            return result;
+        };
+        // execution
+        auto index = GetIndex(arg);
+        switch (index.first) {
+            case 0: return GetName(listFull, index.second);
+            case 1: return GetName(listSmall, index.second);
+            case 2: return GetName(listMedium, index.second);
+        }
+        return std::string("");
+    }
+
 }
 
 namespace PPCGHook {
 
-    template <std::uint8_t mode>
-    std::pair<bool, bool> Process(RE::Actor* a_actor, RE::TESFaction* a_faction, bool a_goToJail, bool a_removeStolenItems) {
+    std::pair<bool, bool> Process(std::string source, RE::Actor* a_actor, RE::TESFaction* a_faction, bool a_goToJail, bool a_removeStolenItems) {
+        REX::INFO("PlayerPayCrimeGold.{} with faction {:08X}", source, a_faction->formID);
         bool goToJail = a_goToJail;
         bool removeStolenItems = a_removeStolenItems;
         bool payCrimeGold = true;
-        // get faction id
-        std::string source = "Unknown";
-        if constexpr (mode == 1) source = "Virtual";
-        if constexpr (mode == 2) source = "Console";
-        if constexpr (mode == 3) source = "Papyrus";
-        std::uint32_t factionID = a_faction->formID;
-        REX::INFO("PlayerPayCrimeGold.{} called, faction FormID is {:X}, processing", source, factionID);
         // load ini
         CSimpleIniA ini;
         ini.SetUnicode();
         SI_Error iniResult = ini.LoadFile("Data\\SFSE\\Plugins\\PlayerPayCrimeGoldTweak.ini");
-        if (iniResult < 0) REX::INFO("Settings file not found, input values: {} / {} / {}", goToJail, removeStolenItems, payCrimeGold);
+        if (iniResult < 0) REX::INFO("Settings file error / Input values = {:X} {:X} {:X}", goToJail, removeStolenItems, payCrimeGold);
         else {
             // general
-            int vanillaFactions = ini.GetLongValue("General", "VanillaFactions", 0);
+            std::string vanillaPlugins = ini.GetValue("General", "VanillaPlugins", 0);
             // modes
-            int goToJailMode = ini.GetLongValue("Modes", "GoToJailMode", 0);
-            int removeStolenItemsMode = ini.GetLongValue("Modes", "RemoveStolenItemsMode", 0);
-            int payCrimeGoldMode = ini.GetLongValue("Modes", "PayCrimeGoldMode", 0);
+            std::int32_t goToJailMode = ini.GetLongValue("Modes", "GoToJailMode", 0);
+            std::int32_t removeStolenItemsMode = ini.GetLongValue("Modes", "RemoveStolenItemsMode", 0);
+            std::int32_t payCrimeGoldMode = ini.GetLongValue("Modes", "PayCrimeGoldMode", 0);
             // values
             bool goToJailValue = ini.GetBoolValue("Values", "GoToJailValue", false);
             bool removeStolenItemsValue = ini.GetBoolValue("Values", "RemoveStolenItemsValue", false);
             bool payCrimeGoldValue = ini.GetBoolValue("Values", "PayCrimeGoldValue", false);
-            // check faction id
-            bool factionIsVanilla = (factionID >> 24) <= vanillaFactions;
+            // check faction
+            bool factionIsVanilla = [](RE::TESFaction* faction, std::string plugins) {
+                auto name = PPCGUtility::GetPluginName(faction);
+                if (auto length = name.size(); length) {
+                    auto list = PPCGUtility::Split(plugins, '|');
+                    for (auto plugin : list)
+                        if (plugin.size() == length)
+                            if (strnicmp(plugin.data(), name.data(), length) == 0)
+                                return true;
+                }
+                return false;
+            }(a_faction, vanillaPlugins);
             // process jail
             if (goToJailMode == 1 && factionIsVanilla) { // replace vanilla
                 goToJail = goToJailValue;
@@ -72,11 +124,11 @@ namespace PPCGHook {
                 payCrimeGold = payCrimeGoldValue;
             }
             // show result
-            REX::INFO("Settings file parsed, result values: {} / {} / {}", goToJail, removeStolenItems, payCrimeGold);
+            REX::INFO("Settings file parsed / Vanilla faction = {:X} / Result values = {:X} {:X} {:X}", factionIsVanilla, goToJail, removeStolenItems, payCrimeGold);
         }
         // allow landing if actor is in space
         if (a_actor->IsInSpace(true) && !goToJail) {
-            std::string command = std::format("CGF \"{}.{}\"", "PlayerPayCrimeGoldTweak", "AllowLanding");
+            std::string command = std::format("CGF {}{}.{}{}", '"', "PlayerPayCrimeGoldTweak", "AllowLanding", '"');
             PPCGUtility::ConsoleExecute(command);
         }
         // keep actor's gold
@@ -91,15 +143,15 @@ namespace PPCGHook {
     private:
         struct PlayerPayCrimeGold {
             static void NEW_V(RE::Actor* a_actor, RE::TESFaction* a_faction, bool a_goToJail, bool a_removeStolenItems) {
-                auto result = Process<1>(a_actor, a_faction, a_goToJail, a_removeStolenItems);
+                auto result = Process("Virtual", a_actor, a_faction, a_goToJail, a_removeStolenItems);
                 return OLD_V(a_actor, a_faction, result.first, result.second);
             }
             static void NEW_C(RE::Actor* a_actor, RE::TESFaction* a_faction, bool a_goToJail, bool a_removeStolenItems) {
-                auto result = Process<2>(a_actor, a_faction, a_goToJail, a_removeStolenItems);
+                auto result = Process("Console", a_actor, a_faction, a_goToJail, a_removeStolenItems);
                 return OLD_C(a_actor, a_faction, result.first, result.second);
             }
             static void NEW_P(RE::Actor* a_actor, RE::TESFaction* a_faction, bool a_goToJail, bool a_removeStolenItems) {
-                auto result = Process<3>(a_actor, a_faction, a_goToJail, a_removeStolenItems);
+                auto result = Process("Papyrus", a_actor, a_faction, a_goToJail, a_removeStolenItems);
                 return OLD_P(a_actor, a_faction, result.first, result.second);
             }
             static inline REL::Relocation<decltype(NEW_V)> OLD_V;
@@ -119,14 +171,10 @@ namespace PPCGHook {
 
 }
 
-namespace PPCGProcess {
-
-    void MessageCallback(SFSE::MessagingInterface::Message* a_msg) noexcept {
-        if (a_msg->type == SFSE::MessagingInterface::kPostLoad) {
-            PPCGHook::PlayerPayCrimeGoldHook::Install();
-        }
+void MessageCallback(SFSE::MessagingInterface::Message* a_msg) noexcept {
+    if (a_msg->type == SFSE::MessagingInterface::kPostLoad) {
+        PPCGHook::PlayerPayCrimeGoldHook::Install();
     }
-
 }
 
 SFSE_PLUGIN_LOAD(const SFSE::LoadInterface* a_sfse) {
@@ -142,7 +190,7 @@ SFSE_PLUGIN_LOAD(const SFSE::LoadInterface* a_sfse) {
     REX::INFO("Starfield v{}", gameInfo);
 
     const auto messagingInterface = SFSE::GetMessagingInterface();
-    if (messagingInterface && messagingInterface->RegisterListener(PPCGProcess::MessageCallback)) {
+    if (messagingInterface && messagingInterface->RegisterListener(MessageCallback)) {
         REX::INFO("Message listener registered");
     } else {
         REX::FAIL("Message listener not registered");
